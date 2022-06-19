35c7e25de864b7c29e23eda064d7139e32dd259f
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-06-19 15:29:18 +0530

    message

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var app=Express()
  app.use(Bodyparser.urlencoded({extended:true}))
  app.use(Bodyparser.json())
 
+ app.get('/',(req,res)=>{
+    res.send(" Welcome to my Bus nodejs")
+})
 app.post('/bus',(req,res)=>{
     var Busobject=new busmodel(req.body)
     res.json(Busobject)
